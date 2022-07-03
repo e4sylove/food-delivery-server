dd3dc0dd4ba216dc37db3a103312b6c0e8bff408
Hieu Nguyen <hieungming@gmail.com>
2022-07-03 17:45:15 +0700

    restructure following clean architecture

--- server/main.go
+++ server/main.go
@@ -1,38 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-
-	"github.com/kataras/iris/v12"
-)
-
-type application struct {
-	config config
-}
-
-type config struct {	
-	port int
-}
-
 func main() {
-	var config config
-	config.port = 8080;
-
-	app := &application{
-		config: config,
-	}
-
-	app.serve()
-}
-
-func (app *application) serve() (error) {
 	
-	server := &http.Server{
-		Addr: fmt.Sprintf(`:%d`, app.config.port),
-	}
-
-	iris := iris.New().Run(iris.Server(server))
-
-	return iris
 }
